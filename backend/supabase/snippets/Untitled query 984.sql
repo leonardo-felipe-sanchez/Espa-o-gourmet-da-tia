8820abb804f5d579b2dd901efda45c8d40f66d54
leonardo-felipe-sanchez <felipinhosanchez77@gmail.com>
2026-06-19 09:15:59 -0300

    asdfgh

--- backend/supabase/snippets/Untitled query 984.sql
+++ backend/supabase/snippets/Untitled query 984.sql
@@ -1,5 +1,20 @@
-INSERT INTO public.produtos (nome, tipo, descricao, imagem, preco) VALUES
-('Pão de mel', 3, '', '', 0.0),
-('Cesta de café da manhã', 3, '', '',  0.0),
-('Torta de morango', 3, '', '', 0.0),
-('Kit festa', 3, '', '', 0.0);
+CREATE POLICY "Usuários autenticados podem criar bolos"
+ON public.bolos
+FOR INSERT
+TO authenticated
+WITH CHECK (auth.uid() = dono_do_bolo);
+
+-- 3️⃣ POLICY: Apenas o DONO pode EDITAR seu bolo
+CREATE POLICY "Usuários podem editar apenas seus próprios bolos"
+ON public.bolos
+FOR UPDATE
+TO authenticated
+USING (auth.uid() = dono_do_bolo)
+WITH CHECK (auth.uid() = dono_do_bolo);
+
+-- 4️⃣ POLICY: Apenas o DONO pode DELETAR seu bolo
+CREATE POLICY "Usuários podem deletar apenas seus próprios bolos"
+ON public.bolos
+FOR DELETE
+TO authenticated
+USING (auth.uid() = dono_do_bolo);
